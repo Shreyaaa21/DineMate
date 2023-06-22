f709d4511b8033fab1e869247d14b27de3971245
Shreya <111531300+Shreyaaa21@users.noreply.github.com>
2023-06-22 15:41:48 +0530

    Add files via upload

---
+++ src/component/basics/menuApi.js
@@ -0,0 +1,140 @@
+// array of objects ,API h ye
+
+const Menu = [
+  {
+    id: 1,
+    image: "images/chhole-bhature.jpeg",
+    name: "chhole-bhature",
+    category: "breakfast",
+    price: "60₹",
+    description:
+      "I love maggi really Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni veniam tenetur provident omnis quae, ea velit at delectus vero.",
+  },
+  {
+    id: 2,
+    image: "images/naan-chicken.jpeg",
+    name: "naan-chicken",
+    category: "lunch",
+    price: "20₹",
+    description:
+      "I love maggi really Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni veniam tenetur provident omnis quae, ea velit at delectus vero.",
+  },
+  {
+    id: 3,
+    image: "images/maggi.jpg",
+    name: "maggi",
+    category: "evening",
+    price: "25₹",
+    description:
+      "I love maggi really Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni veniam tenetur provident omnis quae, ea velit at delectus vero.",
+  },
+  {
+    id: 4,
+    image: "images/puri-sabzi.jpeg",
+    name: "puri-sabzi",
+    category: "dinner",
+    price: "60₹",
+    description:
+      "I love maggi really Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni veniam tenetur provident omnis quae, ea velit at delectus vero.",
+  },
+  {
+    id: 5,
+    image: "images/masaladosa.jpeg",
+    name: "masaladosa",
+    category: "breakfast",
+    price: "20₹",
+    description:
+      "I love maggi really Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni veniam tenetur provident omnis quae, ea velit at delectus vero.",
+  },
+  {
+    id: 6,
+    image: "images/momo.jpeg",
+    name: "momo",
+    category: "evening",
+    price: "20₹",
+    description:
+      "I love maggi really Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni veniam tenetur provident omnis quae, ea velit at delectus vero.",
+  },
+  {
+    id: 7,
+    image: "images/buger&fries.jpeg",
+    name: "burger&fries",
+    category: "evening",
+    price: "20₹",
+    description:
+      "I love maggi really Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni veniam tenetur provident omnis quae, ea velit at delectus vero.",
+  },
+  {
+    id: 8,
+    image: "images/nonvegthali.jpeg",
+    name: "nonvegthali",
+    category: "lunch",
+    price: "80₹",
+    description:
+      "I love maggi really Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni veniam tenetur provident omnis quae, ea velit at delectus vero.",
+  },
+  {
+    id: 9,
+    image: "images/panipuri.jpeg",
+    name: "panipuri",
+    category: "evening",
+    price: "20₹",
+    description:
+      "I love maggi really Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni veniam tenetur provident omnis quae, ea velit at delectus vero.",
+  },
+  {
+    id: 10,
+    image: "images/pav-bhaji.jpeg",
+    name: "pav-bhaji",
+    category: "breakfast",
+    price: "40₹",
+    description:
+      "I love maggi really Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni veniam tenetur provident omnis quae, ea velit at delectus vero.",
+  },
+  {
+    id: 11,
+    image: "images/pizza.jpeg",
+    name: "pizza",
+    category: "evening",
+    price: "150₹",
+    description:
+      "I love maggi really Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni veniam tenetur provident omnis quae, ea velit at delectus vero.",
+  },
+  {
+    id: 12,
+    image: "images/cupcake.jpeg",
+    name: "cake",
+    category: "dessert",
+    price: "20₹",
+    description:
+      "I love maggi really Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni veniam tenetur provident omnis quae, ea velit at delectus vero.",
+  },
+  {
+    id: 13,
+    image: "images/samosa.jpeg",
+    name: "samosa",
+    category: "evening",
+    price: "20₹",
+    description:
+      "I love maggi really Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni veniam tenetur provident omnis quae, ea velit at delectus vero.",
+  },
+  {
+    id: 14,
+    image: "images/vegthali.jpeg",
+    name: "vegthali",
+    category: "breakfast",
+    price: "90₹",
+    description:
+      "I love maggi really Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni veniam tenetur provident omnis quae, ea velit at delectus vero.",
+  },
+  {
+    id: 15,
+    image: "images/sweet.jpeg",
+    name: "sweet",
+    category: "dinner",
+    price: "50₹",
+    description:
+      "I love maggi really Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat magni veniam tenetur provident omnis quae, ea velit at delectus vero.",
+  },
+];
+export default Menu;
